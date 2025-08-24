34d7eba2855657681dc4558efd9f790bf737cfb4
Joachim Uhrlaß <joachim.uhrlass@ecube.de>
2025-08-24 15:47:26 +0200

    Updates

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
-use windows::core::w;
+use std::ffi::c_void;
 use std::mem::size_of;
-
+use windows::core::w;
 use windows::Win32::Foundation::*;
 use windows::Win32::Graphics::Gdi::*;
 use windows::Win32::System::LibraryLoader::GetModuleHandleW;
 use windows::Win32::UI::Shell::*;
 use windows::Win32::UI::WindowsAndMessaging::*;
+
+// Ein 5x7 Pixel Bitmap-Font für die Ziffern 0-9
+const FONT: [[[u8; 5]; 7]; 10] = [
+    // 0
+    [[0,1,1,1,0], [1,0,0,0,1], [1,0,0,0,1], [1,0,0,0,1], [1,0,0,0,1], [1,0,0,0,1], [0,1,1,1,0]],
+    // 1
+    [[0,0,1,0,0], [0,1,1,0,0], [0,0,1,0,0], [0,0,1,0,0], [0,0,1,0,0], [0,0,1,0,0], [0,1,1,1,0]],
+    // 2
+    [[0,1,1,1,0], [1,0,0,0,1], [0,0,0,0,1], [0,0,1,1,0], [0,1,0,0,0], [1,0,0,0,0], [1,1,1,1,1]],
+    // 3
+    [[1,1,1,1,0], [0,0,0,0,1], [0,0,0,1,0], [0,0,1,1,0], [0,0,0,0,1], [1,0,0,0,1], [0,1,1,1,0]],
+    // 4
+    [[0,0,0,1,0], [0,0,1,1,0], [0,1,0,1,0], [1,0,0,1,0], [1,1,1,1,1], [0,0,0,1,0], [0,0,0,1,0]],
+    // 5
+    [[1,1,1,1,1], [1,0,0,0,0], [1,1,1,1,0], [0,0,0,0,1], [0,0,0,0,1], [1,0,0,0,1], [0,1,1,1,0]],
+    // 6
+    [[0,0,1,1,0], [0,1,0,0,0], [1,0,0,0,0], [1,1,1,1,0], [1,0,0,0,1], [1,0,0,0,1], [0,1,1,1,0]],
+    // 7
+    [[1,1,1,1,1], [0,0,0,0,1], [0,0,0,1,0], [0,0,1,0,0], [0,0,1,0,0], [0,0,1,0,0], [0,0,1,0,0]],
+    // 8
+    [[0,1,1,1,0], [1,0,0,0,1], [1,0,0,0,1], [0,1,1,1,0], [1,0,0,0,1], [1,0,0,0,1], [0,1,1,1,0]],
+    // 9
+    [[0,1,1,1,0], [1,0,0,0,1], [1,0,0,0,1], [0,1,1,1,1], [0,0,0,0,1], [0,0,1,0,0], [0,1,1,0,0]],
+];
+
+// Globale Variable für das aktuelle Icon, um es später freigeben zu können
+static mut CURRENT_ICON: HICON = HICON(0 as *mut c_void);
 
 fn main() {
     unsafe {
@@ -21,45 +48,28 @@ fn main() {
         RegisterClassW(&wc);
 
         let hwnd = CreateWindowExW(
-            Default::default(),
-            class_name,
-            w!(""),
-            WS_OVERLAPPEDWINDOW,
-            0,
-            0,
-            0,
-            0,
-            None,
-            None,
-            hinstance,
-            None,
-        )
-        .unwrap();
+            Default::default(), class_name, w!(""), WS_OVERLAPPEDWINDOW,
+            0, 0, 0, 0, None, None, hinstance, None,
+        ).unwrap();
 
         let mut nid = NOTIFYICONDATAW {
             cbSize: size_of::<NOTIFYICONDATAW>() as u32,
-            hWnd: hwnd,
-            uID: 1,
+            hWnd: hwnd, uID: 1,
             uFlags: NIF_MESSAGE | NIF_ICON | NIF_TIP,
             uCallbackMessage: WM_USER + 1,
             ..Default::default()
         };
 
-        nid.hIcon = create_icon_with_text("0,0");
-
-        // KORREKTUR HIER:
-        let tooltip_text: [u16; 9] = [
-            'M' as u16, 'o' as u16, 'u' as u16, 's' as u16, 'e' as u16, 0, 0, 0, 0,
-        ];
-        // 1. Adresse als rohen Pointer holen, ohne Referenz zu erstellen
-        let tip_ptr = std::ptr::addr_of_mut!(nid.szTip);
-        // 2. Den Pointer in den richtigen Typ für die Kopierfunktion umwandeln
-        let tip_ptr_u16 = tip_ptr as *mut u16;
-        // 3. Sicher kopieren
-        std::ptr::copy_nonoverlapping(tooltip_text.as_ptr(), tip_ptr_u16, tooltip_text.len());
+        // Erstes Icon erstellen und setzen
+        CURRENT_ICON = create_icon_with_coords(0, 0);
+        nid.hIcon = CURRENT_ICON;
+        let tooltip_text: [u16; 13] = ['M', 'a', 'u', 's', 'p', 'o', 's', 'i', 't', 'i', 'o', 'n', '\0']
+            .map(|c| c as u16);
+        let tip_ptr = std::ptr::addr_of_mut!(nid.szTip) as *mut u16;
+        std::ptr::copy_nonoverlapping(tooltip_text.as_ptr(), tip_ptr, tooltip_text.len());
 
         Shell_NotifyIconW(NIM_ADD, &mut nid);
-        SetTimer(hwnd, 1, 500, None);
+        SetTimer(hwnd, 1, 100, None); // Timer auf 100ms für flüssigere Updates
 
         let mut msg = MSG::default();
         while GetMessageW(&mut msg, None, 0, 0).into() {
@@ -67,64 +77,87 @@ fn main() {
             DispatchMessageW(&msg);
         }
 
+        // Aufräumen
         Shell_NotifyIconW(NIM_DELETE, &mut nid);
+        if !CURRENT_ICON.is_invalid() {
+            DestroyIcon(CURRENT_ICON);
+        }
     }
 }
 
-unsafe fn create_icon_with_text(text: &str) -> HICON {
+unsafe fn create_icon_with_coords(x_pos: u32, y_pos: u32) -> HICON {
     let hdc = GetDC(None);
     let memdc = CreateCompatibleDC(hdc);
-    let bmp = CreateCompatibleBitmap(hdc, 16, 16);
-    let old = SelectObject(memdc, bmp);
+    let bmp = CreateCompatibleBitmap(hdc, 24, 24);
+    let old_bmp = SelectObject(memdc, bmp);
 
-    PatBlt(memdc, 0, 0, 16, 16, WHITENESS);
+    // Hintergrund schwarz malen
+    PatBlt(memdc, 0, 0, 24, 24, BLACKNESS);
 
-    let wide: Vec<u16> = text.encode_utf16().chain(Some(0)).collect();
-    // KORREKTUR 2: 'as i32' entfernt
-    SetBkMode(memdc, TRANSPARENT);
-    TextOutW(memdc, 0, 0, &wide);
+    // Zahlen vorbereiten
+    let numbers_to_draw = [x_pos % 10000, y_pos % 10000];
+    let text_color = COLORREF(0x0000FF00); // Leuchtendes Grün
 
-    let mut ii = ICONINFO {
-        fIcon: true.into(),
-        xHotspot: 0,
-        yHotspot: 0,
-        hbmMask: bmp,
-        hbmColor: bmp,
-    };
-    // KORREKTUR 3: .unwrap() hinzugefügt
+    // Vertikale Startpositionen
+    let y_positions = [4, 13]; // 4px padding, dann 7px (X) + 2px (gap)
+
+    for (row_idx, number) in numbers_to_draw.iter().enumerate() {
+        let digits = [
+            (number / 1000) % 10,
+            (number / 100) % 10,
+            (number / 10) % 10,
+            *number % 10,
+        ];
+        let start_y = y_positions[row_idx];
+
+        // Zeichne jede Ziffer der Zahl
+        for (i, &digit) in digits.iter().enumerate() {
+            let glyph = FONT[digit as usize];
+            // Horizontale Position für Rechtsbündigkeit (1px Rand links)
+            let start_x = 1 + i as i32 * 6;
+
+            for (y, row) in glyph.iter().enumerate() {
+                for (x, &pixel) in row.iter().enumerate() {
+                    if pixel == 1 {
+                        SetPixel(memdc, start_x + x as i32, start_y + y as i32, text_color);
+                    }
+                }
+            }
+        }
+    }
+
+    let mut ii = ICONINFO { fIcon: true.into(), hbmMask: bmp, hbmColor: bmp, ..Default::default() };
     let hicon = CreateIconIndirect(&mut ii).unwrap();
 
-    SelectObject(memdc, old);
+    SelectObject(memdc, old_bmp);
+    DeleteObject(bmp);
     DeleteDC(memdc);
     ReleaseDC(None, hdc);
 
     hicon
 }
 
-// wndproc bleibt unverändert
-extern "system" fn wndproc(
-    hwnd: HWND,
-    msg: u32,
-    wparam: WPARAM,
-    lparam: LPARAM,
-) -> LRESULT {
+extern "system" fn wndproc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
     unsafe {
         match msg {
             WM_TIMER => {
-                let mut pt = POINT { x: 0, y: 0 };
+                let mut pt = POINT::default();
                 GetCursorPos(&mut pt);
 
-                let text = format!("{},{}", pt.x % 100, pt.y % 100);
+                let new_icon = create_icon_with_coords(pt.x as u32, pt.y as u32);
+
+                if !CURRENT_ICON.is_invalid() {
+                    DestroyIcon(CURRENT_ICON);
+                }
+                CURRENT_ICON = new_icon;
 
                 let mut nid = NOTIFYICONDATAW {
                     cbSize: size_of::<NOTIFYICONDATAW>() as u32,
-                    hWnd: hwnd,
-                    uID: 1,
-                    uFlags: NIF_ICON | NIF_TIP,
+                    hWnd: hwnd, uID: 1,
+                    uFlags: NIF_ICON,
+                    hIcon: CURRENT_ICON,
                     ..Default::default()
                 };
-                nid.hIcon = create_icon_with_text(&text);
-
                 Shell_NotifyIconW(NIM_MODIFY, &mut nid);
             }
             WM_DESTROY => {

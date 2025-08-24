77340566811b29777ebe0863b1010cf02b318a55
Joachim Uhrlaß <joachim.uhrlass@ecube.de>
2025-08-24 22:35:24 +0200

    Fügt Unterstützung für Windows-Ressourcen hinzu, einschließlich der Einbettung eines Icons aus der Datei "assets/logo.ico". Aktualisiert die Abhängigkeiten in Cargo.lock und Cargo.toml, um die neue winres-Bibliothek zu integrieren.

--- build.rs
+++ build.rs
@@ -2,8 +2,15 @@ use embed_manifest::{embed_manifest, new_manifest};
 
 fn main() {
     if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
-        embed_manifest(new_manifest("mpr"))
-            .expect("unable to embed manifest file");
+        embed_manifest(new_manifest("mpr")).expect("unable to embed manifest file");
+
+        let mut res = winres::WindowsResource::new();
+    // Hier den Pfad zu deiner ICO-Datei angeben
+    res.set_icon("assets/logo.ico");
+    if let Err(e) = res.compile() {
+        eprintln!("Fehler beim Kompilieren der Windows-Ressourcen: {}", e);
+    }
+
     }
     println!("cargo:rerun-if-changed=build.rs");
 }

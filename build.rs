533854da9eaf9c91784ef1d992f189a14c0b96ca
Joachim Uhrlaß <joachim.uhrlass@ecube.de>
2025-08-25 00:42:24 +0200

    Entfernt die Logik zum Setzen des Icons mit rcedit aus der build.rs-Datei, um die Build-Konfiguration zu vereinfachen. Dies folgt der vorherigen Entfernung der Windows-Ressourcenkonfiguration und trägt zur Optimierung der Build-Umgebung bei.

--- build.rs
+++ build.rs
@@ -5,33 +5,6 @@ fn main() {
     if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
         embed_manifest(new_manifest("mpr")).expect("unable to embed manifest file");
     }
-    
-    // Nach der Kompilierung: Icon mit rcedit setzen
-    // rcedit muss im PATH verfügbar sein
-    println!("cargo:warning=Versuche Icon mit rcedit zu setzen...");
-    
-    let rcedit_result = Command::new("rcedit")
-        .args(&["target\\release\\mpr.exe", "--set-icon", "logo.ico"])
-        .output();
-    
-    match rcedit_result {
-        Ok(output) => {
-            println!("cargo:warning=rcedit Exit-Code: {}", output.status);
-            println!("cargo:warning=rcedit stdout: {}", String::from_utf8_lossy(&output.stdout));
-            println!("cargo:warning=rcedit stderr: {}", String::from_utf8_lossy(&output.stderr));
-            
-            if output.status.success() {
-                println!("cargo:warning=Icon erfolgreich mit rcedit gesetzt!");
-            } else {
-                println!("cargo:warning=rcedit Fehler: Exit-Code {}", output.status);
-                println!("cargo:warning=Kein Icon wird angehängt!");
-            }
-        }
-        Err(e) => {
-            println!("cargo:warning=rcedit nicht gefunden im PATH: {}", e);
-            println!("cargo:warning=Kein Icon wird angehängt!");
-        }
-    }
-    
+   
     println!("cargo:rerun-if-changed=build.rs");
 }

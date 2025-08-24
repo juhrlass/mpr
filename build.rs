d96eb9f06f31a508ab19c481aa08dcd4df10e2b5
Joachim Uhrlaß <joachim.uhrlass@ecube.de>
2025-08-25 00:11:01 +0200

    Entfernt die Windows-Ressourcenkonfiguration für das Icon aus der build.rs-Datei, um die Build-Umgebung zu vereinfachen. Dies folgt der vorherigen Änderung, die den Pfad zur ICO-Datei aktualisiert hat.

--- build.rs
+++ build.rs
@@ -4,13 +4,6 @@ fn main() {
     if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
         embed_manifest(new_manifest("mpr")).expect("unable to embed manifest file");
 
-        let mut res = winres::WindowsResource::new();
-    // Hier den Pfad zu deiner ICO-Datei angeben
-    res.set_icon("logo.ico");
-    if let Err(e) = res.compile() {
-        eprintln!("Fehler beim Kompilieren der Windows-Ressourcen: {}", e);
-    }
-
     }
     println!("cargo:rerun-if-changed=build.rs");
 }
